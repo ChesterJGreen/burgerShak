ff337046769f39846ac4d68d021ec71a30ce6b5e
cjgreenie <seejarom@gmail.com>
2021-07-26 12:42:23 -0600

    creating fakedb

---
+++ server/db/fakeDb.js
@@ -0,0 +1,15 @@
+export const fakeDb = {
+  burgers: [{
+    id: 1,
+    name: 'Monster Burger',
+    description: "It's huge, and full of meat!"
+  }, {
+    id: 2,
+    name: 'Chicken Burger',
+    description: 'Made from an inferior meat'
+  }, {
+    id: 3,
+    name: 'Kiddie Burger',
+    description: "It's like the monster burger, only a lot smaller"
+  }]
+}
