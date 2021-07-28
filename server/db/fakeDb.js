8884595773830afcaef4516621abbe7d7ba71d14
cjgreenie <seejarom@gmail.com>
2021-07-27 22:45:51 -0600

    good to go

--- server/db/fakeDb.js
+++ server/db/fakeDb.js
@@ -12,4 +12,17 @@ export const fakeDb = {
     name: 'Kiddie Burger',
     description: "It's like the monster burger, only a lot smaller"
   }]
+ frys: [{
+  id: 1,
+  name: "Steak Fries",
+  salt: true
+}, {
+  id: 2,
+  name: "Sweet Potato Fries",
+  salt: true
+}, {
+  id: 3,
+  name: "Onion Fries",
+  salt: false
+}]
 }
